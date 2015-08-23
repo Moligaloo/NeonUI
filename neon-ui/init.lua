6a367a18e24a883b5d6e3d533f0b5b048ef036b8
Moligaloo <moligaloo@gmail.com>
2015-08-24 00:58:15 +0800

    use for loop to adding control class to neon

--- neon-ui/init.lua
+++ neon-ui/init.lua
@@ -1,13 +1,17 @@
 local engine = require 'neon-ui.engine'
 local Control = require 'neon-ui.control'
 
-return {
+local neon = {
 	Control = Control,
-	Button = require 'neon-ui.button',
-	Label = require 'neon-ui.Label',
 
 	engine = engine,
 	overrideHandlers = function(event_names)
 		engine.overrideHandlers(Control.handlers, event_names)
 	end,
 }
+
+for _, class_name in ipairs {'Button', 'Label'} do
+	neon[class_name] = require('neon-ui.' .. class_name:lower())
+end
+
+return neon
