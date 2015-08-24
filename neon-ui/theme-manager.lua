2fc1a20e5d5a46b8f9681f4bbf4090af517599db
Moligaloo <moligaloo@gmail.com>
2015-08-24 20:56:08 +0800

    don't throw error when subconfig is nil

--- neon-ui/theme-manager.lua
+++ neon-ui/theme-manager.lua
@@ -4,6 +4,9 @@ local function getStyle(name, field, state)
 	local config = theme[name]
 	if state then
 		local subconfig = config[state]
+		if subconfig == nil then
+			return getStyle(name, field)
+		end
 		return subconfig[field] and subconfig[field] or getStyle(name, field)
 	end
 
